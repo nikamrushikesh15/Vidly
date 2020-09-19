208ebab2d219ee13630c691cd54e8fbf1b629c02
rushikesh nikam <rushikeshnikam6@gmail.com>
2020-09-19 15:20:48 +0530

    Added movies list and customer list

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,9 +10,13 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
+
+
+
+
         public ActionResult Random()
         {
-            var movie = new Movie() { name="john"};
+            var movie = new Movie() { name = "john" };
             var customer = new List<Customer>
             {
                 new Customer{ name="customer1"},
@@ -23,34 +27,62 @@ namespace Vidly.Controllers
             var viewmodel = new RandomMovieViewModel
             {
                 movie = movie,
-            customer=customer
+                customer = customer
 
             };
 
             return View(viewmodel);
         }
-        public ActionResult Edit(int id) {
+        public ActionResult Edit(int id)
+        {
 
             return Content("id=" + id);
         }
 
-        public ActionResult Index(int? pageindex, string sortby) {
+        public ActionResult Index()
+        {
 
-            if (!pageindex.HasValue)
-                pageindex = 1;
+            //    if (!pageindex.HasValue)
+            //        pageindex = 1;
 
-            if (string.IsNullOrEmpty(sortby)) {
-                sortby = "name";
-            }
+            //    if (string.IsNullOrEmpty(sortby)) {
+            //        sortby = "name";
+            //    }
 
-            return Content(string.Format("pageinxed={0} & sortby={1}", pageindex, sortby));
+            //    return Content(string.Format("pageinxed={0} & sortby={1}", pageindex, sortby));
+            //}
+            //[Route("Movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+            //public ActionResult ByReleasedate(int year,int month) {
+
+            //    return Content("year"+year+"/"+month);
+
+            //}
+
+
+            var Movies = GetMovies();
+
+            return View(Movies);
+
         }
-        [Route("Movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
-        public ActionResult ByReleasedate(int year,int month) {
 
-            return Content("year"+year+"/"+month);
+        public ActionResult Detail(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(c => c.Id == id);
 
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
         }
 
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, name = "raaj" },
+                new Movie { Id = 2, name = "ek thi dyane" }
+            };
+        }
     }
 }
